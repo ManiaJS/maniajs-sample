66b7d1077db95a8ffdd24e0964936c46fd4d9015
Tom Valk <tomvalk@gmail.com>
2016-02-13 15:16:44 +0100

    Adjusting init and injection on sample.

--- plugin.js
+++ plugin.js
@@ -12,14 +12,25 @@ export default class extends Plugin {
   constructor() {
     super();
 
+    // Set the package stuff into the plugin context.
     this.name = Package.name;
     this.version = Package.version;
 
+    // Add dependencies, enter module full id's (mostly npm package names) here.
     this.dependencies = [];
   }
 
+  /**
+   * Init will be run once the plugin can register everything at the core.
+   * From this point the {this.app} and all other injected variables are available.
+   *
+   * @return {Promise} The init should ALWAYS return a promise, the core will wait until the promise has been resolved!
+   */
   init() {
-    console.log("test");
-    this.app.log.debug("Testing loading of plugin.");
+    return new Promise((resolve, reject) => {
+      this.app.log.debug("Testing loading of plugin.");
+
+      return resolve();
+    });
   }
 }

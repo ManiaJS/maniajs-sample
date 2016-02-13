9a0001eea928af1348ccbd30d60aaac34f9076cc
Tom Valk <tomvalk@gmail.com>
2016-02-13 10:55:50 +0100

    Adding inits and sample for creating from abstract plugin.

--- plugin.js
+++ plugin.js
@@ -1,27 +1,19 @@
 'use strict';
 
-var async = require('async');
+import * as Package from './package.json';
 
-module.exports = {};
+import Plugin       from 'maniajs-plugin';
 
-module.exports.pluginInfo = {
-  // TODO: We already have this info in package.json!
-  id: 'maniajs-sample',
-  authors: [
-    "Sample Developer <exampl@example.com>"
-  ],
-  version: "1.0.0",
-  description: "Chat handlers, In and output for chat access.",
+/**
+ * Sample Plugin, For testing and example purpose.
+ */
+export default new Plugin({
+  name: Package.name,
+  version: Package.version,
 
+  // this.app will be the core app interface
 
-  dependencies: [],   // What plugins are needed to operate
-  requirements: [],   // What plugins are required to be started before we can start
-  games: null,        // Define array with list of supported games, null for all (including future games)
-  modes: null         // Define array with list of supported gamemodes, null for all
-};
-
-module.exports.loadPlugin = function(pluginInterface, callback) {
-  setTimeout(function() {
-    return callback(null);
-  }, 1000);
-};
+  init() {
+    this.app.log.debug("Testing");
+  }
+});
